4813cfa8cd78dc3c447153a3731b448a19a399ef
pablaw <chulhwa7@gmail.com>
2022-12-17 16:25:08 +0900

    1st push

---
+++ src/components/SignupInputForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const StBox = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+  margin: 20px;
+`;
+
+const SignupInputForm = () => {
+  return (
+    <div>
+      <StBox>박스</StBox>
+    </div>
+  );
+};
+
+export default SignupInputForm;
